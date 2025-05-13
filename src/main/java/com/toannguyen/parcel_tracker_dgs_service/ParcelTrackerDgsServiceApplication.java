07ba58595a8468e7fff7f18294bbb6da2a500af6
Toan Nguyen <tnguyen@us.maxnerva.com>
2025-05-13 16:07:49 +0700

    init

---
+++ src/main/java/com/toannguyen/parcel_tracker_dgs_service/ParcelTrackerDgsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toannguyen.parcel_tracker_dgs_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelTrackerDgsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelTrackerDgsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toannguyen/parcel_tracker_dgs_service/ParcelTrackerDgsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toannguyen.parcel_tracker_dgs_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelTrackerDgsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
